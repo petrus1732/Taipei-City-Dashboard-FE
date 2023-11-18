cef35c91eb2d8813aaf4d30278a3fe3130eedfd8
str367 <str3670404@gmail.com>
2023-11-19 04:46:20 +0800

    PyramidChart edit

--- src/components/charts/PyramidChart.vue
+++ src/components/charts/PyramidChart.vue
@@ -13,8 +13,8 @@ const props = defineProps([
 const mapStore = useMapStore();
 
 const parsedSeries = computed(() => {
-	const e1 = { name: "2歲名額使用比例", data: [] };
-	const e2 = { name: "3-5歲名額使用比例", data: [] };
+	const e1 = { name: props.chart_config.name[0], data: [] };
+	const e2 = { name: props.chart_config.name[1], data: [] };
 	console.log(props.series[0].data[0]);
 	for (let i = 0; i < props.series[0].data.length; i++) {
 		e1.data.push(
@@ -105,103 +105,13 @@ const chartOptions = ref({
 		},
 	},
 	legend: {
-		offsetX: -10,
-	},
-});
-
-const chartOptions1 = ref({
-	chart: {
-		offsetY: 15,
-		stacked: true,
-		toolbar: {
-			show: false,
-		},
-	},
-	colors: props.chart_config.color,
-	dataLabels: {
-		offsetX: 20,
-		textAnchor: "start",
-	},
-	grid: {
-		show: false,
-	},
-	legend: {
-		show: false,
-	},
-	plotOptions: {
-		bar: {
-			borderRadius: 2,
-			distributed: true,
-			horizontal: true,
-		},
-	},
-	stroke: {
-		colors: ["#282a2c"],
-		show: true,
-		width: 0,
-	},
-	// The class "chart-tooltip" could be edited in /assets/styles/chartStyles.css
-	tooltip: {
-		custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-			return (
-				'<div class="chart-tooltip">' +
-				"<h6>" +
-				w.globals.labels[dataPointIndex] +
-				"</h6>" +
-				"<span>" +
-				series[seriesIndex][dataPointIndex] +
-				` ${props.chart_config.unit}` +
-				"</span>" +
-				"</div>"
-			);
-		},
-		followCursor: true,
-	},
-	xaxis: {
-		axisBorder: {
-			show: false,
-		},
-		axisTicks: {
-			show: false,
-		},
-		labels: {
-			show: false,
-		},
-		type: "category",
-	},
-	yaxis: {
-		labels: {
-			formatter: function (value) {
-				return value.length > 7 ? value.slice(0, 6) + "..." : value;
-			},
-		},
+		offsetX: -50,
 	},
 });
 
 const chartHeight = computed(() => {
 	return `${40 + props.series[0].data.length * 24}`;
 });
-
-const selectedIndex = ref(null);
-
-function handleDataSelection(e, chartContext, config) {
-	if (!props.chart_config.map_filter) {
-		return;
-	}
-	if (config.dataPointIndex !== selectedIndex.value) {
-		mapStore.addLayerFilter(
-			`${props.map_config[0].index}-${props.map_config[0].type}`,
-			props.chart_config.map_filter[0],
-			props.chart_config.map_filter[1][config.dataPointIndex]
-		);
-		selectedIndex.value = config.dataPointIndex;
-	} else {
-		mapStore.clearLayerFilter(
-			`${props.map_config[0].index}-${props.map_config[0].type}`
-		);
-		selectedIndex.value = null;
-	}
-}
 </script>
 
 <template>
